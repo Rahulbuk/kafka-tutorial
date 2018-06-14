573d0d670e8f9e4ce9cfc780d1a8ac7e033b7959
bwardzinski <bwardzinski@pl.imshealth.com>
2018-06-14 11:24:26 +0200

    Refactoring and kafka-streams module init

---
+++ kafka-streams/src/main/java/com/wardziniak/kafka/streams/basic/SampleStreamApp.java
@@ -0,0 +1,13 @@
+package com.wardziniak.kafka.streams.basic;
+
+import com.google.gson.internal.Streams;
+
+import java.util.concurrent.CountDownLatch;
+
+public class SampleStreamApp {
+
+    public static void main(String[] args) {
+
+
+    }
+}
